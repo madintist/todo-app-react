dff9a224918e2b9025fecd7f2cf0e8fa4efaec46
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 09:02:04 -0500

    Added render to TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,6 +3,14 @@ import './TodoList.css';
 
 const todoListComponent = Object.create(Component.prototype);
 
+todoListComponent.render = function () {
+  return (
+    <ul>
+      {/* Todo list items here */}
+    </ul>
+  );
+};
+
 const TodoList = () => todoListComponent;
 
 export default TodoList;
