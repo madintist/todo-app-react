c0ee36296720ae43a6537d33cce17e6174b9a55c
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 09:00:03 -0500

    Added TodoList

---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './TodoList.css';
+
+const todoListComponent = Object.create(Component.prototype);
+
+const TodoList = () => todoListComponent;
+
+export default TodoList;
