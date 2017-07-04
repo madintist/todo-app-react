26412b6da60585a64f5e0d8eca17a983e35a3b52
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 08:49:50 -0500

    Added render to PageHeader

--- src/PageHeader.js
+++ src/PageHeader.js
@@ -3,4 +3,10 @@ import './PageHeader.css';
 
 const PageHeader = Object.create(Component.prototype);
 
+PageHeader.render = function (props) {
+  return (
+      <h1>{ props.headerText }</h1>
+  );
+};
+
 export default () => PageHeader;
