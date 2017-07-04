e7f48f009af61189bd251f6c566c221e3851407a
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 09:14:01 -0500

    Added basic code to TodoListItem

---
+++ src/TodoListItem.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './TodoListItem.css';
+
+const todoListItemComponent = Object.create(Component.prototype);
+
+const TodoListItem = () => todoListItemComponent;
+
+export default TodoListItem;
