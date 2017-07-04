b3993ef340a5d06fa6b35b628907f0d2c85194ea
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 09:40:47 -0500

    Added CompleteItemButton boilerplate

---
+++ src/CompleteItemButton.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './CompleteItemButton.css';
+
+const completeItemButtonComponent = Object.create(Component.prototype);
+
+const CompleteItemButton = () => completeItemButtonComponent;
+
+export default CompleteItemButton;
