3a2b0678545b28ee0163d54140f3eb1432c2b97e
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 09:16:14 -0500

    Added render()

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -3,6 +3,15 @@ import './TodoListItem.css';
 
 const todoListItemComponent = Object.create(Component.prototype);
 
+todoListItemComponent.render = function (props) {
+  return (
+    <li>
+      {props.todoItem.title}
+      {/* Complete button here */}
+    </li>
+  );
+};
+
 const TodoListItem = () => todoListItemComponent;
 
 export default TodoListItem;
