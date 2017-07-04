263b4e13028874102e052adbb2ba656037361e39
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 09:44:29 -0500

    Added render()

--- src/CompleteItemButton.js
+++ src/CompleteItemButton.js
@@ -3,6 +3,14 @@ import './CompleteItemButton.css';
 
 const completeItemButtonComponent = Object.create(Component.prototype);
 
+completeItemButtonComponent.render = function () {
+  return (
+    <button onClick={/* complete item */}>
+      <i class="fa fa-check" aria-hidden="true"></i>
+    </button>
+  );
+};
+
 const CompleteItemButton = () => completeItemButtonComponent;
 
 export default CompleteItemButton;
