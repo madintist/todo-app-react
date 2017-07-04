290968845fd332cab4cb6b22ec04946030169a4a
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 08:39:41 -0500

    Added render function

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import './Todo.css';
 
-const Todo = Object.create(Component.prototype);
+const todoComponent = Object.create(Component.prototype);
+
+todoComponent.render = function () {
+  return (
+    <div className="todo-container">
+      {/* Page header here */}
+      {/* Todo list here */}
+    </div>
+  );
+};
+
+const Todo = () => todoComponent;
 
 export default Todo;
