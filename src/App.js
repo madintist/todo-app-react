ad6d23e339613821748f9ca99576d8a468a0c274
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 10:51:53 -0500

    Link to Font Awesome

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App () {
   return (
     <div className="App">
       <Todo />
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
     </div>
   );
 }
