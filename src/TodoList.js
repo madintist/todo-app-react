fb70fa85e1b75f21ee340901b715d2319130018b
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-04 10:29:24 -0500

    Refactored TodoList into a class component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,46 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 
-const todoListComponent = Object.create(Component.prototype);
+class TodoList extends React.Component {
+  constructor () {
+    super();
 
-todoListComponent.state = {
-  todoItems: [
-    {
-      id: 0,
-      title: 'Item 1',
-      complete: false
-    },
-    {
-      id: 1,
-      title: 'Item 2',
-      complete: false
-    },
-    {
-      id: 2,
-      title: 'Item 3',
-      complete: false
-    },
-    {
-      id: 3,
-      title: 'Item 4',
-      complete: false
-    }
-  ]
-};
+    this.state = {
+      todoItems: [
+        {
+          id: 0,
+          title: 'Item 1',
+          complete: false
+        },
+        {
+          id: 1,
+          title: 'Item 2',
+          complete: false
+        },
+        {
+          id: 2,
+          title: 'Item 3',
+          complete: false
+        },
+        {
+          id: 3,
+          title: 'Item 4',
+          complete: false
+        }
+      ]
+    };
+  }
 
-todoListComponent.incompleteItems = todoListComponent.state.todoItems.map(function (item) {
-  return <TodoListItem key={item.id} todoTitle={item.title} />;
-});
+  incompleteItems () {
+    return this.state.todoItems.map(function (item) {
+      return <TodoListItem key={item.id} todoTitle={item.title} />;
+    });
+  }
 
-todoListComponent.render = function () {
-  return (
-    <ul>
-      {this.incompleteItems}
-    </ul>
-  );
-};
-
-const TodoList = () => todoListComponent;
+  render () {
+    return (
+      <ul>
+        {this.incompleteItems}
+      </ul>
+    );
+  }
+}
 
 export default TodoList;
